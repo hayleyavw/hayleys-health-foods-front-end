3d3d8e686a07b970f76a7ca8ee839648c856e932
Hayley van Waas <hayleyavw@gmail.com>
2019-08-30 15:50:20 +0800

    fixing directory structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom'
 import './App.css';
 import HomePage from './pages/HomePage'
 import BlogPage from './pages/BlogPage';
 import StyleGuide from './pages/StyleGuide';
+import { Switch, Route } from 'react-router-dom';
 
 
 const App: React.FC = () => {
