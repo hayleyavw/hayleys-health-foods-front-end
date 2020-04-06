0280d0f5844630bd8473c94a82f43fc1e3a971b2
Hayley van Waas <hayleyavw@gmail.com>
2020-04-06 21:36:35 +0800

    WIP styling contact form.

--- src/components/ContactForm/ContactForm.styled.tsx
+++ src/components/ContactForm/ContactForm.styled.tsx
@@ -1,1 +1,60 @@
 import styled from '@emotion/styled'
+import { calcRem } from '../styling/styling-utils/calcRem'
+import { colours } from '../styling/colours'
+
+export const StyledForm = styled('form')({
+    width: '100%',
+})
+
+export const StyledFieldset = styled('fieldset')({
+    display: 'block',
+    padding: calcRem(9),
+    border: `${calcRem(1)} solid ${colours.primary.primary}`,
+    marginBottom: calcRem(30),
+    borderRadius: calcRem(5),
+    // &.empty {
+    //     > span {
+    //         display: block;
+    //     }
+    // }
+    '&:last-child': {
+        border: 'none',
+        marginBottom: 0,
+        textAlign: 'center',
+    },
+    ' > label': {
+        display: 'block',
+        float: 'left',
+        marginTop: calcRem(-28),
+        background: colours.white,
+        height: calcRem(14),
+        padding: calcRem(7, 5, 7, 5),
+        color: colours.primary.primary,
+        fontSize: calcRem(18),
+        overflow: 'hidden',
+    },
+    // .form-element.disabled > label {
+    //     color: $secondaryColor;
+    // }
+
+    ' > span': {
+        backgroundColor: colours.primary.primary,
+        display: 'none',
+        padding: calcRem(3),
+        margin: calcRem(0, -9, -9, -9),
+        textAlign: 'center',
+        color: colours.white,
+        fontSize: calcRem(18),
+    },
+    // .form-element.disabled > span {
+    //     background-color: $secondaryColor;
+    // }
+})
+
+export const StyledLabel = styled('label')({
+    width: calcRem(100),
+})
+
+export const StyledInput = styled('input')({})
+
+export const StyledTextarea = styled('textarea')({})
--- src/components/ContactForm/ContactForm.tsx
+++ src/components/ContactForm/ContactForm.tsx
@@ -1,28 +1,35 @@
 import React from 'react'
+import {
+    StyledForm,
+    StyledLabel,
+    StyledInput,
+    StyledTextarea,
+    StyledFieldset,
+} from './ContactForm.styled'
 
 class ContactForm extends React.Component {
     render() {
         return (
-            <form action="https://formspree.io/xpzjpbap" method="POST">
-                <fieldset>
-                    <label htmlFor="formName">Name</label>
-                    <input name="name" id="name-input" type="text" required />
-                </fieldset>
+            <StyledForm action="https://formspree.io/xpzjpbap" method="POST">
+                <StyledFieldset>
+                    <StyledLabel htmlFor="formName">Name</StyledLabel>
+                    <StyledInput name="name" id="name-input" type="text" required />
+                </StyledFieldset>
 
-                <fieldset>
-                    <label htmlFor="formEmail">Email</label>
-                    <input name="email" id="email-input" type="email" required />
-                </fieldset>
+                <StyledFieldset>
+                    <StyledLabel htmlFor="formEmail">Email</StyledLabel>
+                    <StyledInput name="email" id="email-input" type="email" required />
+                </StyledFieldset>
 
-                <fieldset>
-                    <label htmlFor="formMessage">Message</label>
-                    <textarea name="message" id="message-input" required></textarea>
-                </fieldset>
+                <StyledFieldset>
+                    <StyledLabel htmlFor="formMessage">Message</StyledLabel>
+                    <StyledTextarea name="message" id="message-input" required></StyledTextarea>
+                </StyledFieldset>
 
                 <div>
                     <input type="submit" placeholder="Send message" />
                 </div>
-            </form>
+            </StyledForm>
         )
     }
 }
