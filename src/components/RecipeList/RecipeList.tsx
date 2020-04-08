1f7c5bb4c72d54042ab9d7d0eb1c9c1bce454d51
Hayley van Waas <hayleyavw@gmail.com>
2020-04-08 22:23:37 +0800

    Added loading animation to lists.

--- src/components/RecipeList/RecipeList.tsx
+++ src/components/RecipeList/RecipeList.tsx
@@ -4,39 +4,58 @@ import { StyledRecipeList } from './RecipeList.styled'
 import { getRecipeGraphQL } from '../../api/recipes/Queries'
 import { RecipeGraphQLObject } from '../../api/recipes/ResponseShapes'
 import { api_url } from '../../api/common'
+import Helmet from 'react-helmet'
+import { jsonld } from '../common/jsonld'
+import { StyledContentBox } from '../common/ContentBox.styled'
+import Loading from '../Loading/Loading'
 
 interface State {
     recipes: RecipeGraphQLObject[]
+    loading: boolean
 }
 
 export class RecipeList extends React.Component {
     public readonly state: Readonly<State> = {
         recipes: [new RecipeGraphQLObject()],
+        loading: true,
     }
 
-    componentDidMount() {
-        getRecipeGraphQL({}).then(recipe => {
-            this.setState({ recipes: recipe })
-        })
+    async componentDidMount() {
+        try {
+            await getRecipeGraphQL({}).then(recipe => {
+                this.setState({ recipes: recipe })
+                this.setState({ loading: false })
+            })
+        } catch {
+            this.setState({ loading: false })
+        }
     }
 
     render() {
         return (
-            <StyledRecipeList>
-                {this.state.recipes.map((recipe: RecipeGraphQLObject, index) => (
-                    <Card
-                        key={index}
-                        url={`/recipes/${recipe.slug}`}
-                        title={recipe.title}
-                        description={recipe.description}
-                        heroURL={
-                            process.env.NODE_ENV !== 'production'
-                                ? `${api_url}/${recipe.hero.url}`
-                                : recipe.hero.url
-                        }
-                    />
-                ))}
-            </StyledRecipeList>
+            <React.Fragment>
+                {this.state.loading ? (
+                    <React.Fragment>
+                        <Loading />
+                    </React.Fragment>
+                ) : (
+                    <StyledRecipeList>
+                        {this.state.recipes.map((recipe: RecipeGraphQLObject, index) => (
+                            <Card
+                                key={index}
+                                url={`/recipes/${recipe.slug}`}
+                                title={recipe.title}
+                                description={recipe.description}
+                                heroURL={
+                                    process.env.NODE_ENV !== 'production'
+                                        ? `${api_url}/${recipe.hero.url}`
+                                        : recipe.hero.url
+                                }
+                            />
+                        ))}
+                    </StyledRecipeList>
+                )}
+            </React.Fragment>
         )
     }
 }
