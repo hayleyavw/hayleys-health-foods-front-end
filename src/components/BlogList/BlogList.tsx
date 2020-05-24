948aca2bd15f5fa9bcc00d086b7b6d592f84e457
Hayley van Waas <hayleyavw@gmail.com>
2020-05-24 16:15:28 +0800

    Removed moment js.

--- src/components/BlogList/BlogList.tsx
+++ src/components/BlogList/BlogList.tsx
@@ -36,14 +36,12 @@ export class BlogList extends React.Component {
                         <StyledHeadingTwo>Blog</StyledHeadingTwo>
                         <StyledBlogList>
                             {this.state.blogPosts.map(function(blogPost, index) {
-                                const moment = require('moment')
-                                const createdAt = moment(blogPost.createdAt).format('Do MMM YYYY')
                                 return (
                                     <Card
                                         key={index}
                                         url={`/blog/${blogPost.slug}`}
                                         title={blogPost.title}
-                                        subtitle={createdAt}
+                                        subtitle={blogPost.createdAt}
                                         thumbnail={blogPost.thumbnail.url}
                                         description={blogPost.content}
                                         page={'Blog List Page'}
